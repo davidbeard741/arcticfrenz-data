b56f81dc75dd1fe87032a961ed55c75e20888df2
anymouse <53785044+davidbeard741@users.noreply.github.com>
2023-12-02 08:30:51 -0500

    Update chickentribe.py

--- chickentribe.py
+++ chickentribe.py
@@ -68,6 +68,7 @@ def driversetup(logger):
     options.add_argument("--incognito")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.page_load_strategy = 'normal'
+    options.binary_location = '/usr/bin/google-chrome'
 
     user_agents = [
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
@@ -80,7 +81,7 @@ def driversetup(logger):
 
     caps = webdriver.DesiredCapabilities.CHROME
     caps['goog:loggingPrefs'] = {'browser': 'ALL'}
-    service = webdriver.chrome.service.Service('/usr/bin/chromedriver')
+    # service = webdriver.chrome.service.Service('/usr/bin/chromedriver')
     driver = webdriver.Chrome(options=options)
     driver.execute_cdp_cmd("Network.clearBrowserCookies", {})
     driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"Referer": "https://www.google.com/"}})
