934d5ee97551e7d36d935e3514142d45cdfc598c
anymouse <53785044+davidbeard741@users.noreply.github.com>
2023-12-02 23:11:24 -0500

    Update histogram.py

--- chickentribe/histogram.py
+++ chickentribe/histogram.py
@@ -6,32 +6,39 @@ file_path = '/mnt/data/69.json'
 with open(file_path, 'r') as file:
     nft_data = json.load(file)
 
-def plot_nft_ownership_histogram(nft_data, bins=30):
+def adjusted_plot_nft_ownership_histogram(nft_data, bins=30):
     
-    def calculate_ownership_duration(acquired_timestamp, checked_timestamp):
+    def calculate_ownership_duration(acquired_timestamp):
         acquired_date = datetime.utcfromtimestamp(acquired_timestamp)
-        checked_date = datetime.utcfromtimestamp(checked_timestamp)
-        return (checked_date - acquired_date).days
+        current_date = datetime.now()
+        return (current_date - acquired_date).days
 
     ownership_durations = []
+
     for nft in nft_data:
-        holder_data = nft.get('holder data', [])
-        when_acquired = next((item['when acquired'] for item in holder_data if 'when acquired' in item), None)
-        time_checked = next((item['time checked'] for item in holder_data if 'time checked' in item), None)
-        if when_acquired and time_checked:
-            duration = calculate_ownership_duration(when_acquired, time_checked)
+        holder_data_list = nft.get('holder data', [])
+        
+        holder = ""
+        when_acquired = None
+        for item in holder_data_list:
+            if 'holder' in item:
+                holder = item['holder']
+            elif 'when acquired' in item:
+                when_acquired = item['when acquired']
+        
+        if holder != "Magic Eden V2 Authority" and when_acquired:
+            duration = calculate_ownership_duration(when_acquired)
             if duration >= 0:
                 ownership_durations.append(duration)
 
     plt.style.use('dark_background')
-
     plt.figure(figsize=(12, 8))
     n, bins, patches_hist = plt.hist(ownership_durations, bins=bins, color='#0D47A1', edgecolor='#B3E5FC')
 
     max_bin_index = n.argmax()
     patches_hist[max_bin_index].set_facecolor('#FFC107')
 
-    plt.title('NFT Ownership Duration Distribution', fontsize=16, color='#E0E0E0')
+    plt.title('Adjusted NFT Ownership Duration Distribution', fontsize=16, color='#E0E0E0')
     plt.xlabel('Length of Ownership (Days)', fontsize=14, color='#E0E0E0')
     plt.ylabel('Number of NFTs', fontsize=14, color='#E0E0E0')
     plt.xticks(fontsize=12, color='#E0E0E0')
@@ -42,6 +49,6 @@ def plot_nft_ownership_histogram(nft_data, bins=30):
 
     plt.grid(axis='y', alpha=0.65, color='#E0E0E0')
 
-    plt.show()
+    plt.savefig('/mnt/data/adjusted_nft_ownership_histogram.png', format='png')
 
-plot_nft_ownership_histogram(nft_data)
+adjusted_plot_nft_ownership_histogram(nft_data)
