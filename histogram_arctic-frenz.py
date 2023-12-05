f945709e1c6bf445de16062830a8801ec86c56e9
anymouse <53785044+davidbeard741@users.noreply.github.com>
2023-12-05 06:36:15 -0500

    Update histogram_arctic-frenz.py

--- histogram_arctic-frenz.py
+++ histogram_arctic-frenz.py
@@ -8,7 +8,7 @@ with open(file_path, 'r') as file:
     nft_data = json.load(file)
 
 def plot_nft_ownership_histogram(nft_data, bins=30):
-    
+
     def calculate_ownership_duration(acquired_timestamp):
         acquired_date = datetime.utcfromtimestamp(acquired_timestamp)
         current_date = datetime.utcnow()
@@ -22,12 +22,11 @@ def plot_nft_ownership_histogram(nft_data, bins=30):
             return False
 
     ownership_durations = []
-
     for nft in nft_data:
         holder_data = nft.get('holderData', {})
         holder = holder_data.get('holderAddress', "")
         when_acquired = holder_data.get('whenAcquired', None)
-        
+
         if holder != "Magic Eden V2 Authority" and when_acquired and is_valid_unix_timestamp(when_acquired):
             duration = calculate_ownership_duration(when_acquired)
             if duration >= 0:
@@ -36,7 +35,7 @@ def plot_nft_ownership_histogram(nft_data, bins=30):
     plt.style.use('dark_background')
     fig, ax = plt.subplots(figsize=(10, 8))
     plt.subplots_adjust(bottom=0.25)
-    
+
     n, bins, patches_hist = ax.hist(ownership_durations, bins=bins, color='#0D47A1', edgecolor='#B3E5FC')
 
     max_bin_indices = np.argsort(n)[-3:]
@@ -47,16 +46,24 @@ def plot_nft_ownership_histogram(nft_data, bins=30):
     ax.set_title('Arctic Frenz\nNFT Ownership Duration Distribution', fontsize=16, color='#E0E0E0')
     ax.set_xlabel('Length of Ownership (Days)', fontsize=14, color='#E0E0E0')
     ax.set_ylabel('Number of NFTs', fontsize=14, color='#E0E0E0')
+
     ax.tick_params(axis='x', colors='#E0E0E0', labelsize=12)
     ax.tick_params(axis='y', colors='#E0E0E0', labelsize=12)
 
     ax.text(0.5, 0.5, 'Arctic Frenz', fontsize=70, color='gray', alpha=0.2,
              ha='center', va='center', rotation=30, transform=ax.transAxes)
-		
+
     generation_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
     footnote_text = f"Chart generated on: {generation_time}\nThis analysis did not include NFTs listed on Magic Eden."
     fig.text(0.05, 0.1, footnote_text, fontsize=10, color='#E0E0E0')
 
     ax.grid(axis='y', alpha=0.65, color='#E0E0E0')
 
-    plt.savefig('arcticfrenz/histogram-ownership-duration_arctic-frenz.png', format='png', bbox_inches='tight')
+    try:
+        plt.savefig(file_path, format='png', bbox_inches='tight')
+        plt.close(fig)
+        print(f"File saved successfully at {file_path}")
+    except Exception as e:
+        print(f"Error saving file: {e}")
+
+plot_nft_ownership_histogram(nft_data)
