87037483a524c28f1b24826c65b3b22760d0bd94
anymouse <53785044+davidbeard741@users.noreply.github.com>
2023-12-13 21:10:14 -0500

    Update by-nft_chickentribe.py

--- by-nft_chickentribe.py
+++ by-nft_chickentribe.py
@@ -34,7 +34,7 @@ def rewrite_data(data):
     })
   return rewritten_data
 
-with open("chickentribe/with_rarity_and_holder_data.json") as f:
+with open("chickentribe/with-rarity-and-holder-data.json") as f:
   data = json.load(f)
 
 naming = set([get_name(x["onChainMetadata"]["metadata"]["data"]["name"]) for x in data])
